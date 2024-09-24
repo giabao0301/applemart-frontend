18b670ac68249494a6d2906ff3d59162c6f553a6
giabao0301 <21521866@gm.uit.edu.vn>
2024-09-24 14:07:44 +0700

    Refactor ProductDetail component and add quantity input

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -7,13 +7,14 @@ import {
   ProductItem,
   Variation,
 } from "@/types/product";
-import { Button } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import formatPrice from "@/utils/priceFormatter";
 import { notFound, useRouter } from "next/navigation";
 import slugify from "@/utils/slugConverter";
+import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
 interface Props {
   product: Product;
@@ -28,6 +29,7 @@ const ProductDetail: React.FC<Props> = ({ product, productItem }) => {
   console.log("rendering product detail");
 
   const router = useRouter();
+  const [quantity, setQuantity] = useState<string>("1");
   const [selectedOptions, setSelectedOptions] = useState<SelectedOptions>({});
 
   const { isPending, error, data } = useQuery({
@@ -101,7 +103,8 @@ const ProductDetail: React.FC<Props> = ({ product, productItem }) => {
 
     if (isAllOptionsSelected) {
       const slug = slugify(Object.values(selectedOptions).join(" "));
-      router.push(`/${product.parentCategory}/${product.slug}/${slug}`);
+
+      router.replace(`/${product.parentCategory}/${product.slug}/${slug}`);
     }
   }, [
     optionNames,
@@ -125,9 +128,49 @@ const ProductDetail: React.FC<Props> = ({ product, productItem }) => {
 
   const image = productItem ? productItem.imageUrl : product.images[0].url;
   const price = productItem ? productItem.price : product.lowestPrice;
+  const quantityInStock = productItem ? productItem.quantity : 0;
+
+  if (isPending) return <div>Loading...</div>;
+
+  if (error) {
+    console.log("Error fetching product items: ", error);
+    return <div>Error fetching product items</div>;
+  }
+
+  const quantityChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === "") {
+      return setQuantity("");
+    }
+
+    const value = parseInt(e.target.value);
+
+    if (value > quantityInStock) {
+      return setQuantity(quantityInStock.toString());
+    } else {
+      return setQuantity(value.toString());
+    }
+  };
+
+  const changeQuantityHandler = (action: string) => {
+    if (action === "increase") {
+      setQuantity((prev) => {
+        const value = parseInt(prev) + 1;
+        if (value > quantityInStock) return prev;
+        return value.toString();
+      });
+    }
+
+    if (action === "decrease") {
+      setQuantity((prev) => {
+        const value = parseInt(prev) - 1;
+        if (value < 1) return prev;
+        return value.toString();
+      });
+    }
+  };
 
   return (
-    <div className="flex flex-wrap mb-[50px] mx-auto w-[912px]">
+    <div className="flex flex-wrap mb-[50px] mx-auto w-[980px]">
       <div className="basis-[45.83333%] max-w-[45.83333%] mr-[8.33333%]">
         <div className="w-full h-auto">
           <Image
@@ -169,14 +212,14 @@ const ProductDetail: React.FC<Props> = ({ product, productItem }) => {
         </div>
       </div>
       <div className="flex flex-col justify-between basis-[45.83333%] max-w-[45.83333%]">
-        <div className="min-h-28">
+        <div className="min-h-20">
           {productItem ? (
             <h1 className="text-3xl font-semibold">{productItem.sku}</h1>
           ) : (
             <h1 className="text-4xl font-semibold">{product.name}</h1>
           )}
         </div>
-        <div className="mt-8">
+        <div className="mt-6">
           {colors && (
             <>
               <div className="font-semibold">
@@ -207,7 +250,7 @@ const ProductDetail: React.FC<Props> = ({ product, productItem }) => {
           {groupedVariations &&
             groupedVariations.map((variation) => (
               <div key={variation.id}>
-                <div className="mt-8 font-semibold">{variation.name}</div>
+                <div className="mt-6 font-semibold">{variation.name}</div>
                 <ul className="flex gap-8 pt-[18px]">
                   {variation.options.map((option) => (
                     <li
@@ -228,10 +271,39 @@ const ProductDetail: React.FC<Props> = ({ product, productItem }) => {
               </div>
             ))}
         </div>
-        <div className="my-8">
+        <div className="my-6">
           <span className="text-xl leading-6 font-semibold">
             {formatPrice(price)}đ
           </span>
+        </div>
+        <div className="flex mb-7 gap-8">
+          <div className="flex border-2 items-center justify-center space-x-2 rounded-full p-2 w-32 bg-white text-[#222222] shadow-[0_10px_65px_-10px_rgba(0,0,0,0.25)]">
+            <button
+              className="text-gray-500 font-bold text-xl hover:opacity-75"
+              onClick={() => changeQuantityHandler("decrease")}
+            >
+              <MinusIcon color="#000" />
+            </button>
+            <input
+              type="number"
+              id="number-input"
+              pattern="[0-9]+"
+              className="text-center w-12 outline-none bg-transparent"
+              value={quantity}
+              onChange={quantityChangeHandler}
+            />
+            <button
+              className="text-gray-500 font-bold text-xl hover:opacity-75"
+              onClick={() => changeQuantityHandler("increase")}
+            >
+              <PlusIcon color="#000" />
+            </button>
+          </div>
+
+          <div className="flex gap-2 items-center text-gray-500">
+            <span>{quantityInStock}</span>
+            <p>sản phẩm có sẵn</p>
+          </div>
         </div>
         <Button
           type="submit"
